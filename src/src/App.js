db334b5f445465b57464d483c5501c96b7ea137b
Ezequiel Alexandre <ezequiel@tecsharp.com.br>
2021-01-07 00:46:07 -0300

    todo list 1

--- src/src/App.js
+++ src/src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      newItem: "",
+      list: []
+    }
+  }
+
+  deleteItem(id) {
+    //copy current list of item
+    const list = [...this.state.list];
+
+    //filter ot item being delted
+    const updatedList = list.filter(item => item.id !== id)
+
+    this.setState({ list: updatedList });
+  }
+
+  updateInput(key, value) {
+    //update react state
+    this.setState({
+      [key]: value
+    })
+  }
+
+  addItem() {
+    //create item with unique id
+    const newItem = {
+      id: 1 + Math.random(),
+      value: this.state.newItem.slice()
+    };
+
+    //copy of current list
+    const list = [...this.state.list];
+
+    //add new item to list
+    list.push(newItem);
+
+    //update state with list and reset newItem input
+    this.setState({
+      list,
+      newItem: ""
+    })
+  }
+  render() {
+    return (
+      <div>
+        adiciona um item...
+        <br />
+        <input
+          type="text"
+          placeholder="Digite uma tarefa..."
+          value={this.state.newItem}
+          onChange={e => this.updateInput("newItem", e.target.value)}
+        />
+
+        <button
+          onClick={() => this.addItem()}
+        > Adicionar </button>
+
+        <ul>
+          {this.state.list.map(item => {
+            return (
+              <li key={item.id}>
+                {item.value}
+                <button
+                  onClick={() => this.deleteItem(item.id)}>x</button>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    )
+  }
 }
-
 export default App;
